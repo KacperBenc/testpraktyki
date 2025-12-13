597a15b5ea93111fab4188354ac01c0b1cb1dbc1
Arkadiusz Wiącek <wiacek.arkadiusz@gmail.com>
2025-12-13 19:03:34 +0100

    Poprawia błąd scieżki to 'static'

--- system_zarzadzania_praktykami/settings.py
+++ system_zarzadzania_praktykami/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'aplikacjaTest/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "aplikacjaTest/static",
 ]
 
 # Default primary key field type

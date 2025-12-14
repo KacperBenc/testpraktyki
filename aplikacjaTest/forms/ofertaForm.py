bbc349510df8e83295a6c55bd41745b322673df2
Arkadiusz Wiącek <wiacek.arkadiusz@gmail.com>
2025-12-14 02:01:58 +0100

    Dodaje filtorwanie w szablonie listy użytkowników

--- aplikacjaTest/forms/ofertaForm.py
+++ aplikacjaTest/forms/ofertaForm.py
@@ -1,7 +1,20 @@
 from django import forms
 from ..models import Oferta
+from aplikacjaTest.models import Uzytkownik
+
+ROLA_CHOICES = Uzytkownik.Role.choices
 
 class OfertaForm(forms.ModelForm):
     class Meta:
         model = Oferta
         fields = ["pracodawca", "opis", "rodzaj_zgloszenia", "dostepnosc_oferty"]
+
+class UzytkownikFilterForm(forms.Form):
+    rola = forms.MultipleChoiceField(
+        choices=ROLA_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple  # albo forms.SelectMultiple
+    )
+    login = forms.CharField(required=False)
+    adres_mailowy = forms.CharField(required=False)
+    numer_telefonu = forms.CharField(required=False)
--- aplikacjaTest/views/przegladajUzytkownikow.py
+++ aplikacjaTest/views/przegladajUzytkownikow.py
@@ -2,11 +2,33 @@ from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from aplikacjaTest.models import Uzytkownik
+from aplikacjaTest.forms.ofertaForm import UzytkownikFilterForm
 
 
 class UzytkownikListView(LoginRequiredMixin, PermissionRequiredMixin, View):
-    permission_required = "aplikacjaTest.view_students"  # app_label + codename
+    permission_required = "aplikacjaTest.view_students"
 
     def get(self, request):
-        uzytkownicy = Uzytkownik.objects.all()
-        return render(request, "uzytkownicy_lista.html", {"uzytkownicy": uzytkownicy})
+        qs = Uzytkownik.objects.all()
+        form = UzytkownikFilterForm(request.GET or None)
+
+        if form.is_valid():
+            role = form.cleaned_data.get("rola")
+            login = form.cleaned_data.get("login")
+            email = form.cleaned_data.get("adres_mailowy")
+            telefon = form.cleaned_data.get("numer_telefonu")
+
+            if role:
+                qs = qs.filter(rola__in=role)
+            if login:
+                qs = qs.filter(login__icontains=login)
+            if email:
+                qs = qs.filter(adres_mailowy__icontains=email)
+            if telefon:
+                qs = qs.filter(numer_telefonu__icontains=telefon)
+
+        context = {
+            "uzytkownicy": qs,
+            "ofertaForm": form,
+        }
+        return render(request, "uzytkownicy_lista.html", context)

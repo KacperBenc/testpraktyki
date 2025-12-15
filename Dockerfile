0b51963bb207b45cb0bddbba21df20f5ddf10c1f
Arkadiusz Wiącek <wiacek.arkadiusz@gmail.com>
2025-12-15 02:08:57 +0100

    Konfiguruje projekt pod Docker

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,30 @@
 FROM python:3.12-slim
 
-# Ustawienia środowiska Pythona
 ENV PYTHONDONTWRITEBYTECODE=1 \
-    PYTHONUNBUFFERED=1
+    PYTHONUNBUFFERED=1 \
+    DJANGO_SETTINGS_MODULE=system_zarzadzania_praktykami.settings \
+    DEBUG=False
 
-# Katalog roboczy
 WORKDIR /app
 
-# Systemowe zależności (np. dla psycopg2, Pillow)
+# Systemowe zależności
 RUN apt-get update && apt-get install -y \
     build-essential \
     libpq-dev \
     && rm -rf /var/lib/apt/lists/*
 
-# Instalacja zależności Pythona
+# Zależności Pythona
 COPY requirements.txt /app/
 RUN pip install --upgrade pip && \
     pip install --no-cache-dir -r requirements.txt
 
-# Kopia kodu projektu
+# Kod projektu
 COPY . /app/
 
-COPY . .
-ENV DJANGO_SETTINGS_MODULE=python.settings
-RUN python manage.py migrate
-RUN python manage.py init_roles
-RUN python manage.py seed_app_praktyki
+# Zbieranie statyk (już z DEBUG=False)
+RUN python manage.py collectstatic --noinput
 
-# Port dla runserver
 EXPOSE 8000
 
-# Komenda startowa (dev)a
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+# Gunicorn jako serwer WSGI
+CMD ["gunicorn", "system_zarzadzania_praktykami.wsgi:application", "--bind", "0.0.0.0:8000"]
--- system_zarzadzania_praktykami/settings.py
+++ system_zarzadzania_praktykami/settings.py
@@ -10,21 +10,28 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+import environ
+
 from pathlib import Path
 from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env(
+    DEBUG=(bool, False),
+)
+
+environ.Env.read_env(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!uzmcthg(dnf%nhekgg#ehl#a3-pms*r#%x4*a_1686t4)^01x'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "praktyki.prod", "praktyki.local", "praktyki"]
 
@@ -111,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl-pl'
 
 TIME_ZONE = 'UTC'
 
@@ -123,10 +130,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'aplikacjaTest/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "aplikacjaTest/static",
-]
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
